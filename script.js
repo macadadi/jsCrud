0d15b6280c04ec0145223c4b717aa45e829a4ac8
macadadi <macadadi36@gmail.com>
2021-03-20 16:55:40 +0300

    crud app

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-let selectedRow = null
- formSubmit() {
-	validate();
+
+
+function formSubmit() {
+	if(validate()){
+		console.log(2)
+		let formData = readFormData();
+		if(selectedRow == null){
+			insertNewRecord(formData)
+		}else{
+			console.log(1)
+			updateRecord(formData)
+		}
+		resetForm()
+	}
 	// body...
 }
 
@@ -38,5 +49,41 @@ function validate(){
  }
 
  function insertNewRecord(data){
- 	
+ 	let table = document.getElementById('employeeList').getElementByTagName('tbody')[0];
+ 	let newRow = table.insertRow(table.length);
+ 	cell1 = newRow.insertCell(0);
+ 	cell1.innerHTML =data.fullName;
+ 	cell2 = newRow.insertCell(1);
+ 	cell2.innerHTML = data.email;
+ 	cell3 = newRow.insertCell(2)
+ 	cell3.innerHTML = data.city
+ 	cell4 = newRow.insertCell(3);
+ 	cell4.innerHTML = data,salary
+ 	cell5 = newRow.insertCell(4)
+ 	cell5.innerHTML = "<a onClick='onEdit(this)'>Edit </a> <a onClick='onDelete(this)'> delete </a>"
+
+ }
+
+ function onEdit(td){
+ 	selectedRow = td.parentElement.parentElement;
+ 	document.getElementById('fullName').value = selectedRow.cell[0].innerHTML
+ 	document.getElementById('email').value = selectedRow.cell[1].innerHTML
+ 	document.getElementById('city').value = selectedRow.cell[2].innerHTML
+ 	document.getElementById('salary').value = selectedRow.cell[3].innerHTML
+ }
+
+
+ function updateRecord(formData){
+ 	selectedRow.cell[0].innerHTML = formData.fullName
+ 	selectedRow.cell[1].innerHTML = formData.email
+ 	selectedRow.cell[2].innerHTML = formData.city
+ 	selectedRow.cell[3].innerHTML = formData.salary
+ }
+
+ function onDelete(){
+ 	if(confirm('are you sure')){
+ 		row = td.parentElement.parentElement;
+ 		document.getElementById('employeeList').deleteRow(row.rowIndex)
+ 		resetForm()
+ 	}
  }
